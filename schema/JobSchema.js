168fcc23056caf036812f5098120607390ca3479
samdsk <sam.ds@live.com>
2024-04-23 17:57:11 +0200

    Reapply "Merge branch 'bugfix/db_id' into dev"
    This reverts commit a4c7f089a347b2c35691ba86e228d3bda43dacf4.

--- schema/JobSchema.js
+++ schema/JobSchema.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const jobSchema = new mongoose.Schema({
   _id: String,
+  id: String, // original job record's id (NOT unique)
   jobType: String,
   title: String,
   company: String,
@@ -12,10 +13,10 @@ const jobSchema = new mongoose.Schema({
   timeAgoPosted: String,
   salaryRange: String,
   jobProviders: [{ jobProvider: String, url: String }],
-  searchDate: Date,
-  searchJobType: String,
-  searchLocation: String,
-  searchLanguage: String,
+  searchDate: Date, // search query executed date
+  searchJobType: String, // search query job type
+  searchLocation: String, // search query location
+  searchLanguage: String, // search query language
 });
 
 module.exports = mongoose.model("Job", jobSchema);
