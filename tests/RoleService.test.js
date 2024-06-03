02eaca4e72404172bc45aa799ab3881f74ba153e
Sam. K. <sam.ds@live.com>
2024-06-03 18:22:25 +0200

    RoleService, AnnotatorService, AnnotationService

---
+++ tests/RoleService.test.js
@@ -0,0 +1,63 @@
+const RoleService = require("../lib/RoleService");
+const TransactionWrapper = require("../lib/TransactionWrapper");
+const { Role } = require("../schemas/Role");
+
+const AnnotatorService = require("../lib/AnnotatorService");
+
+describe("Role Service", () => {
+  it("create a role", async () => {
+    const spyRole = jest
+      .spyOn(Role, "create")
+      .mockImplementation(async () => Promise.resolve());
+
+    const role = {};
+    await RoleService.create(role);
+
+    expect(spyRole).toHaveBeenCalled();
+  });
+
+  it("update Reliability score", async () => {
+    const spyRole = jest
+      .spyOn(Role, "updateOne")
+      .mockImplementation(async () => Promise.resolve());
+
+    const id = "id";
+    const score = 56;
+
+    await RoleService.updateReliabilityScore(id, score);
+
+    expect(spyRole).toHaveBeenCalled();
+    expect(spyRole).toHaveBeenCalledWith(
+      { _id: id },
+      { reliability_score: score }
+    );
+  });
+
+  it("delete Role cascades deletes to Annotators with session", async () => {
+    const spyRole = jest
+      .spyOn(Role, "deleteOne")
+      .mockImplementation(async () => Promise.resolve());
+
+    const spyAnnotator = jest
+      .spyOn(AnnotatorService, "deleteAnnotators")
+      .mockImplementation(async () => Promise.resolve());
+
+    const id = "id";
+    const session = "session";
+    await RoleService.deleteRole(id, session);
+
+    expect(spyRole).toHaveBeenCalledWith({ _id: id }, { session: session });
+    expect(spyAnnotator).toHaveBeenCalledWith({ role: id }, session);
+  });
+
+  it("delete Role cascades deletes to Annotators without session", async () => {
+    const spyTransactionWrapper = jest
+      .spyOn(TransactionWrapper, "transactionWrapper")
+      .mockImplementation(async () => Promise.resolve());
+
+    const id = "id";
+    await RoleService.deleteRole(id);
+
+    expect(spyTransactionWrapper).toHaveBeenCalled();
+  });
+});
