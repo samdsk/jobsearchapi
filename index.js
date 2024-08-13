dffb5f98865d6edd0d78605b7bc534c99dd1336b
Sam. K. <sam.ds@live.com>
2024-08-13 15:32:52 +0200

    scheduler

--- index.js
+++ index.js
@@ -1,66 +1,55 @@
 require("dotenv").config();
 
 const { db_connect, db_close } = require("./db/db");
-const Utils = require("./lib/utils");
-const Automate = require("./lib/Automaters/RapidAPIAutomater");
+const RapidAPIAutomator = require("./lib/Automators/RapidAPIAutomator");
 const { logResultsToJSONFile } = require("./lib/resultsLogger");
 const Logger = require("./lib/logger");
-const Collector = require("./lib/Collectors/RapidAPICollector");
-const SearchRequestSender = require("./lib/RequestSenders/RapiAPIRequestSender");
-const JobPostController = require("./Controllers/JobPostController");
-const RapidAPIConverter = require("./lib/Converters/RapidAPIConverter");
-const JobPostService = require("./Services/JobPostService");
 
-const automate = async () => {
+const CollectorEventEmitter = require("./lib/CollectorEventEmitter");
+const { Scheduler, EVENT } = require("./lib/Scheduler");
+
+const app = async () => {
+  const schedulerExpression = "*/1 * * * *"; // every 1 minutes
+  const keyset1 = new Set([
+    "***REMOVED***",
+  ]);
+
+  const jobList1 = ["sviluppatore software"];
+
+  const Emitter = new CollectorEventEmitter();
+
+  Emitter.on(EVENT, wrapper.bind(null, keyset1, jobList1));
+
+  const scheduler = new Scheduler(Emitter, schedulerExpression);
+
+  scheduler.start();
+
+  setTimeout(async () => {
+    scheduler.stop();
+    await db_connect();
+    Logger.info("Exiting...");
+  }, 1000 * 60 * 3 + 1000 * 20);
+};
+
+const wrapper = async (keySet, jobList) => {
+  const automator = new RapidAPIAutomator(keySet);
+  const response = await automator.collect(jobList);
+
+  Logger.info("Logging results summary");
+  await logResultsToJSONFile("summary", new Date(Date.now()), response);
+  Logger.info(JSON.stringify(response));
+};
+
+async function start() {
   try {
-    const filename_jobtypes = process.argv[2] || "jobtypeslist.json";
-    Logger.info(`Reading job types file : ${filename_jobtypes}`);
-    const jobTypes = await Utils.getJobTypesFromFile(filename_jobtypes);
-
-    const filename_keys = process.argv[3] || "keylist.json";
-    Logger.info(`Reading key list file : ${filename_keys}`);
-    const keys = await Utils.getJSONFromFile(filename_keys);
-
-    const keySet = new Set(keys);
-    const automate = new Automate(keySet);
-
     Logger.info("Starting...");
     await db_connect();
-    const response = await automate.collect(jobTypes);
-
-    Logger.info("Logging results summary");
-    await logResultsToJSONFile("summary", new Date(Date.now()), response);
-
-    Logger.info(JSON.stringify(response));
-
-    console.log(response);
-
-    await db_close();
-    Logger.info("Exiting...");
-
-    process.exit();
+    await app();
   } catch (error) {
     await db_close();
-    Logger.error(JSON.stringify(error));
     console.error(error);
     process.exit(1);
   }
-};
+}
 
-const single = async () => {
-  const sender = new SearchRequestSender();
-  const controller = new JobPostController(RapidAPIConverter, JobPostService);
-  const collector = new Collector(sender, controller);
-  try {
-    Logger.info("Starting...");
-
-    await db_connect();
-    const response = await collector.searchJobsByType("Software Engineer");
-    Logger.debug(response);
-    await db_close();
-    Logger.info("Exiting...");
-  } catch (err) {
-    Logger.debug(err);
-    process.exit(1);
-  }
-};
+start();
