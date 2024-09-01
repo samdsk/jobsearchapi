f4645e45f25a688981ef21d98c949114df79fce8
Sam. K. <sam.ds@live.com>
2024-09-01 15:23:14 +0200

    User schema

---
+++ schemas/User.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const USER_TYPES = ["Admin", "AnnotationService", "LLM"]
+const validator = require('validator');
+
+const User = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: {
+                validator: validator.isEmail
+            }
+        },
+        password: {
+            type: String,
+            required: true,
+            min: 6,
+        },
+        userType: {
+            type: String,
+            required: true,
+            enum: USER_TYPES
+        }
+    },
+    {timestamps: true}
+);
+
+User.index({email: 1}, {unique: true});
+
+module.exports = {User, USER_TYPES};
